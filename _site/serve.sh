3ee87440fab1832e30d1a1a57d9006b69d47f7bd
mean-machine-gc <chiodi@unicc.org>
2026-02-21 19:19:10 +0100

    remove _site folder....

--- _site/serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install json-schema-for-humans --quiet
-generate-schema-doc --config-file schema/jsfh-config.yaml schema/lifecycle.v1.schema.json schema/lifecycle-docs.html
-generate-schema-doc --config-file schema/jsfh-config.yaml schema/process.v1.schema.json schema/process-docs.html
-bundle install
-bundle exec jekyll serve --host 0.0.0.0
